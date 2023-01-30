98a8144e035ec28a0ce452d34feacb307a297a99
Bruce Mayenschein <brucedmayenschein@gmail.com>
2023-01-30 14:32:24 -0500

    Piano class added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-let notes = document.querySelectorAll(".note");
+class Piano {
+    constructor() {
+        this.notes = document.querySelectorAll(".note");
+    }
 
-notes.forEach(note => note.addEventListener('click', playNote));
-
-function playNote(ev) {
-    let note = ev.target.dataset.note
-    console.log(note);
-    let audio = document.querySelector(`audio[data-note=${note}]`)
-    audio.play();
-    console.log(audio);
+    setNotes() {
+        this.notes.forEach(note => note.addEventListener('click', this.playNote));
+    }
+    playNote(ev) {
+        let note = ev.target.dataset.note;
+        let audio = document.querySelector(`audio[data-note=${note}]`);
+        audio.play()
+    }
 }
+
+
