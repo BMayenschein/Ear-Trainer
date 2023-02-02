9f44c5afd67ae020d1d8833b20542d401b9b1849
Bruce Mayenschein <brucedmayenschein@gmail.com>
2023-02-02 11:02:57 -0500

    added Piano and Training class with basic functionality

--- js/main.js
+++ js/main.js
@@ -1,16 +1,53 @@
 class Piano {
     constructor() {
         this.notes = document.querySelectorAll(".note");
+        //Lookup table with format of {note:<audio>}
+        this.noteTable = {};
+        this.notes.forEach(note => {
+            this.noteTable[note.dataset.note] = note.querySelector('audio');
+        });
     }
-
-    setNotes() {
-        this.notes.forEach(note => note.addEventListener('click', this.playNote));
-    }
-    playNote(ev) {
-        let note = ev.target.dataset.note;
-        let audio = document.querySelector(`audio[data-note=${note}]`);
-        audio.play()
+    playNote(note) {
+        this.noteTable[note].play()
     }
 }
 
+class Training {
+    constructor() {
+        this.piano = new Piano();
+        this.startButton = document.querySelector(".start");
+        this.startButton.addEventListener('click', () => {
+            this.startTraining();
+        });
+        this.piano.notes.forEach(note => note.addEventListener('mousedown', (ev) => {
+            train.piano.playNote(ev.target.dataset.note);
+        }))
+    }
+
+    randomNote() {
+        let allNotes = ['A', 'As', 'B', 'C', 'Cs', 'D', 'Ds', 'E', 'F', 'Fs', 'G', 'Gs']
+        let randomNum = Math.floor(Math.random() * allNotes.length);
+        let randNote = allNotes[randomNum];
+        let correctNote = document.querySelector(`[data-note=${randNote}]`);
+
+        this.piano.playNote(randNote);
+        return [randNote, correctNote]
+    }
+
+    startTraining() {
+        let values = this.randomNote();
+        let randomNote = values[0];
+        let correctNote = values[1];
+
+        let correctClick = function() {
+            console.log('Correct!');
+            correctNote.removeEventListener('click', correctClick);
+        }
+        correctNote.addEventListener('click', correctClick);
+        console.log(randomNote);
+        console.log(correctNote);
+    }
+}
+
+let train = new Training();
 
